fce8cc7175111434914b5013005675ddf54ea68b
Daniel Joyce <daniel.a.joyce@gmail.com>
2019-12-07 10:51:10 -0800

    Starting to work, but no icon

--- scc/gui/statusicon.py
+++ scc/gui/statusicon.py
@@ -263,6 +263,8 @@ class StatusIconAppIndicator(StatusIconDBus):
 		StatusIcon.__init__(self, *args, **kwargs)
 		
 		try:
+			import gi
+			gi.require_version('AppIndicator3', '0.1')
 			from gi.repository import AppIndicator3 as appindicator
 			
 			self._status_active  = appindicator.IndicatorStatus.ACTIVE
@@ -312,8 +314,8 @@ class StatusIconProxy(StatusIcon):
 		try:
 			# Try loading GTK native status icon
 			self._status_gtk = StatusIconGTK3(*args, **kwargs)
-			self._status_gtk.connect(b"clicked",        self._on_click)
-			self._status_gtk.connect(b"notify::active", self._on_notify_active_gtk)
+			self._status_gtk.connect("clicked",        self._on_click)
+			self._status_gtk.connect("notify::active", self._on_notify_active_gtk)
 			self._on_notify_active_gtk()
 			
 			log.info("Using backend StatusIconGTK3 (primary)")
@@ -322,7 +324,7 @@ class StatusIconProxy(StatusIcon):
 			self._load_fallback()
 	
 	def _on_click(self, *args):
-		self.emit(b"clicked")
+		self.emit("clicked")
 	
 	def _on_notify_active_gtk(self, *args):
 		if self._status_fb:
@@ -350,8 +352,8 @@ class StatusIconProxy(StatusIcon):
 			for StatusIconBackend in status_icon_backends:
 				try:
 					self._status_fb = StatusIconBackend(*self._arguments[0], **self._arguments[1])
-					self._status_fb.connect(b"clicked",        self._on_click)
-					self._status_fb.connect(b"notify::active", self._on_notify_active_fb)
+					self._status_fb.connect("clicked",        self._on_click)
+					self._status_fb.connect("notify::active", self._on_notify_active_fb)
 					self._on_notify_active_fb()
 					
 					log.warning("StatusIcon: Using backend %s (fallback)" % StatusIconBackend.__name__)
